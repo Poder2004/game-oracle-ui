dc0152199e7b0b02bb4ba134e7d551375e200f0a
Chonrattiz <66011212083@msu.ac.th>
2025-10-02 19:10:11 +0700

    เพิ่ม

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,19 @@ import { Routes } from '@angular/router';
 import { Register } from './page/register/register';
 import { Login } from './page/login/login';
 import { Main } from './page/main/main';
+import { Mainadmin } from './admin/mainadmin/mainadmin';
+import { Addgame } from './admin/addgame/addgame';
+import { Historyuser } from './admin/history/history';
+import { Discounts } from './admin/discounts/discounts';
 
 export const routes: Routes = [
-     { path: 'login', component: Login },
-    { path: 'register', component: Register },
+  { path: 'login', component: Login },
+  { path: 'register', component: Register },
 
-    // (แนะนำ) กำหนดหน้าเริ่มต้น เมื่อไม่มี path
-    { path: '', component: Main  }  
+  // (แนะนำ) กำหนดหน้าเริ่มต้น เมื่อไม่มี path
+  { path: '', component: Main },
+  { path: 'Mainadmin', component: Mainadmin },
+  { path: 'addgame', component: Addgame },
+  { path: 'history', component: Historyuser },
+  { path: 'discounts', component: Discounts },
 ];
