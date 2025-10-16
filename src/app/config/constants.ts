634b7fc728098177caf5d929546e61c484043563
poder <66011212129@gmail.com>
2025-10-16 23:42:43 +0700

    grbjh,

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -6,5 +6,5 @@ import { Injectable } from '@angular/core';
 export class Constants {
   // public readonly API_ENDPOINT: string =
   //   'https://api-project-adv-web.onrender.com';
-  public readonly API_ENDPOINT: string = 'http://localhost:8080';
+  public readonly API_ENDPOINT: string = 'https://api-project-adv-web.onrender.com';
 }
